62abb440ef238569f30b2e9340dcc7c3b2e6febe
Cuba49 <kubovych.kostya59@gmail.com>
2016-11-14 12:24:23 +0200

    залиття лаби

---
+++ OOP_lab1/OOP_lab1/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlTypes;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OOP_lab1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int[] mas = new int[Convert.ToInt32(textBox1.Text)];
+
+            Random r = new Random();
+            for (int i = 0; i < mas.Length; i++)
+            {
+                mas[i] = r.Next(Convert.ToInt32(textBox2.Text), Convert.ToInt32(textBox3.Text));
+            }
+            double sum = 0;
+            Form columnsAndRows = new Form2(mas);
+            columnsAndRows.Show();
+        }
+    }
+}
